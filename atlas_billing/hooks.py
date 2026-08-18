80fa33b294ec70e37df466973107fef4f11f6ae2
Leniel <lenielmr@gmail.com>
2026-08-18 19:23:13 -0400

    US-20: Require a reason on manual journal entry adjustments

--- atlas_billing/hooks.py
+++ atlas_billing/hooks.py
@@ -36,6 +36,7 @@ fixtures = [
 	},
 	{"dt": "Item Group", "filters": [["name", "in", ["Capilar", "Cuidado facial", "Masaje"]]]},
 	{"dt": "Item Attribute", "filters": [["name", "=", "Longitud de pelo"]]},
+	{"dt": "Property Setter", "filters": [["name", "=", "Journal Entry-user_remark-reqd"]]},
 ]
 
 doctype_js = {
